cdc28b76ee65bfc48eb8a044e0d3db6727a98ada
Harun Smrkovic <harun@personal.com>
2014-11-17 03:13:43 +0100

    font awesome issue resolved

--- Gruntfile.js
+++ Gruntfile.js
@@ -341,7 +341,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/{,*/}*.*'
+            'fonts/{,*/}*.*',
+            'bower_components/font-awesome/fonts/{,*/}*.*'
           ]
         }, {
           expand: true,
